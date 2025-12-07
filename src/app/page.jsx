aa168f2f7cc5008bffa35e8468784ce8a9509630
azryl <azryl.career@gmail.com>
2025-12-07 18:19:34 +0800

    Add mock data layer and render homepage sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,13 @@
+import rooms from "@/data/rooms.json";
+
 export default function Home() {
   return (
     <>
-      <h1>STEM Pulse</h1>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <h3>{room.name}</h3>)
+      ) : (
+        <p>No room available at the moment</p>
+      )}
     </>
   );
 }
