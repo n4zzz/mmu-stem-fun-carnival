776f02e5d74eeeba0755f87fc8a879c2e8655cb6
azryl <azryl.career@gmail.com>
2025-12-07 18:47:26 +0800

    Create RoomCard component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,11 @@
 import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
 
 export default function Home() {
   return (
     <>
       {rooms.length > 0 ? (
-        rooms.map((room) => <h3>{room.name}</h3>)
+        rooms.map((room) => <RoomCard room={room}/>)
       ) : (
         <p>No room available at the moment</p>
       )}
