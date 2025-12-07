bd61377a119be2be5578a3ff5860f72e72e4ed41
azryl <azryl.career@gmail.com>
2025-12-07 19:04:12 +0800

    Create Heading component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,11 @@
 import rooms from "@/data/rooms.json";
+import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
 
 export default function Home() {
   return (
     <>
+    <Heading title='Available Labs'></Heading>
       {rooms.length > 0 ? (
         rooms.map((room) => <RoomCard room={room}/>)
       ) : (
