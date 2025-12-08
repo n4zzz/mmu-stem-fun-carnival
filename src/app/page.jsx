a9bf7e04a21c03bb8ecdae4c7a70be915b3f2caa
azryl <azryl.career@gmail.com>
2025-12-09 03:24:47 +0800

    Implement getAllRooms and connect to homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,16 @@
-import rooms from "@/data/rooms.json";
+// import rooms from "@/data/rooms.json";
 import Heading from "@/components/Heading";
 import RoomCard from "@/components/RoomCard";
+import getAllRooms from "./actions/getAllRooms";
 
-export default function Home() {
+export default async function Home() {
+  const rooms = await getAllRooms();
+
   return (
     <>
-    <Heading title='Available Labs'></Heading>
+      <Heading title='Available Labs'></Heading>
       {rooms.length > 0 ? (
-        rooms.map((room) => <RoomCard room={room}/>)
+        rooms.map((room) => <RoomCard room={room} />)
       ) : (
         <p>No room available at the moment</p>
       )}
