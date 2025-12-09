d5e9f228d408f6405e0279595c84385b764343cb
n4zzz <nazrinadanan02@gmail.com>
2025-12-09 20:55:23 +0800

    update resource card button aesthethic

--- src/components/RoomCard.jsx
+++ src/components/RoomCard.jsx
@@ -15,22 +15,23 @@ const RoomCard = ({ room }) => {
                 <div className="space-y-1">
                     <h4 className="text-lg font-semibold">{room.name}</h4>
                     <p className="text-sm text-gray-600">
-                        <span className="font-semibold text-gray-800"> Address: </span> 
+                        <span className="font-semibold text-gray-800"> Address:</span> 
                         {room.address}
                     </p>
                     <p className="text-sm text-gray-600">
-                        <span className="font-semibold text-gray-800"> Availability: </span>
+                        <span className="font-semibold text-gray-800"> Availability:</span>
                         {room.availability}
-                    </p>
-                    <p className="text-sm text-gray-600">
-                        <span className="font-semibold text-gray-800"> Price: </span>
-                        ${room.price_per_hour}/hour
                     </p>
                 </div>
             </div>
+            
             <div className="flex flex-col sm:flex-row w-full sm:w-auto sm:space-x-2 mt-2 sm:mt-0">
-                <Link href={`/rooms/${room.$id}`} className="bg-blue-500 text-white px-4 py-2 rounded mb-2 sm:mb-0 w-full sm:w-auto text-center hover:bg-blue-700">
-                    View Resource</Link>
+                <Link 
+                    href={`/rooms/${room.$id}`} 
+                    className="block w-full text-center bg-indigo-50 text-indigo-700 py-2 px-4 rounded-lg font-medium hover:bg-indigo-100 transition-colors mt-4 sm:mt-0"
+                >
+                    View Resource
+                </Link>
             </div>
         </div>
     );
