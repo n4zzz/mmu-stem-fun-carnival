72de321da07485c4a415877320fc6cc6f86b87bd
n4zzz <nazrinadanan02@gmail.com>
2025-12-09 18:56:59 +0800

    syntax update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,19 @@
-  // import rooms from "@/data/rooms.json";
-  import Heading from "@/components/Heading";
-  import RoomCard from "@/components/RoomCard";
-  import getAllRooms from "./actions/getAllRooms";
+// import rooms from "@/data/rooms.json";
+import Heading from "@/components/Heading";
+import RoomCard from "@/components/RoomCard";
+import getAllRooms from "./actions/getAllRooms";
 
-  export default async function Home() {
-    const rooms = await getAllRooms();
+export default async function Home() {
+  const rooms = await getAllRooms();
 
-    return (
-      <>
-        <Heading title='Available Labs'></Heading>
-        {rooms.length > 0 ? (
-          rooms.map((room) => <RoomCard room={room} />)
-        ) : (
-          <p>No room available at the moment</p>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <Heading title='Available Labs'></Heading>
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (
+        <p>No room available at the moment</p>
+      )}
+    </>
+  );
+}
